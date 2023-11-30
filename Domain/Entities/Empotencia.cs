8bf3615631a9de2dce342064da049d1240605c6a
Julio Cesar Tadeu dos Santos da Silva <julio.c.tadeu@gmail.com>
2023-11-30 15:28:44 -0300

    Ajustes em entidades

--- Domain/Entities/Empotencia.cs
+++ Domain/Entities/Empotencia.cs
@@ -1,14 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Questao5.Domain.Entities
 {
     public class Empotencia
     {
+        [Required]
         public string IdEmpotencia { get; set; }
+        
+        [StringLength(1000)]
         public string Requisicao { get; set; }
+        
+        [StringLength(1000)]
         public string Resultado { get; set; }
     }
 }
